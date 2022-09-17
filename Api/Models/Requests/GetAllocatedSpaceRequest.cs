8483720004be0a08fac6fb8724f7e70ce0f73ce5
kpringal <43990727+kpringal@users.noreply.github.com>
2022-09-18 01:02:51 +0530

    adding date range for space allocation selection

---
+++ Api/Models/Requests/GetAllocatedSpaceRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Models.Requests
+{
+    public class GetAllocatedSpaceRequest
+    {
+        public Guid UserId { get; set; }
+        public DateTime StartAllocationDateTime { get; set; }
+        public DateTime EndAllocationDateTime { get; set; }
+
+        public override string ToString()
+        {
+            return $"{UserId} {StartAllocationDateTime} {EndAllocationDateTime}";
+        }
+    }
+}
