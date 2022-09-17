fe53777d5820a33386f3877ca3ed74bb8e8f11f2
kpringal <43990727+kpringal@users.noreply.github.com>
2022-09-17 18:24:43 +0530

    Update AllocatedSpaceResponse.cs

--- Api/Models/Responses/AllocatedSpaceResponse.cs
+++ Api/Models/Responses/AllocatedSpaceResponse.cs
@@ -23,5 +23,6 @@ namespace Api.Models.Responses
         public Guid OfficeFloorDetailKey { get; set; }
         public Guid OfficeSeatDetailKey { get; set; }
         public Guid UserOfficeSeatAllocationDetailKey { get; set; }
+        public string SpaceAllocatedTo { get; set; }
     }
 }
