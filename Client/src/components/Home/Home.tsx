16af89f4eeec40b147dfae14b9f8ac06493c1ee3
RajeshVermaNITK2010 <44517298+RajeshVermaNITK2010@users.noreply.github.com>
2022-09-18 18:38:55 +0530

    Welcome

--- Client/src/components/Home/Home.tsx
+++ Client/src/components/Home/Home.tsx
@@ -45,7 +45,7 @@ function Home() {
       <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
         <Toolbar>
           <Typography variant="h6" noWrap component="div">
-            Hi {localStorage.getItem("userName")}
+            Welcome {localStorage.getItem("userName")}
           </Typography>
         </Toolbar>
         
