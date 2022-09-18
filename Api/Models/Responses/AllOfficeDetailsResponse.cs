e63d989a581448240a5dec60863f3aead2aafdde
RajeshVermaNITK2010 <44517298+RajeshVermaNITK2010@users.noreply.github.com>
2022-09-18 16:10:19 +0530

    GetAllOfficeDetails service implemented

---
+++ Api/Models/Responses/AllOfficeDetailsResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Api.Models.Responses
+{
+    public class AllOfficeDetailsResponse
+    {
+        public List<OfficeDetial> AllOfficeDetails { get; set; }
+        public string Comment { get; set; }
+        public bool HasError { get; set; }
+    }
+    public class OfficeDetial
+    {
+        public string OfficeName { get; set; }
+        public string FloorName { get; set; }
+        public string ZoneName { get; set; }
+        public int SeatCount { get; set; }
+    }    
+}
